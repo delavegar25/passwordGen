24362b6104c2f0aa6f5ff2933ee1fa414d538029
Pascal <pascalokereke18@gmail.com>
2023-09-10 20:24:32 +0100

    appcont

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ const genBtn = document.querySelector('.btn1');
 const copyBtn = document.querySelector('.btn2');
 let password = document.getElementById('password');
 
+genBtn.addEventListener('click', () => genPassword());
+copyBtn.addEventListener('click', () => copyPassword());
