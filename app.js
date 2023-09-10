0b71786df46df9b8eb15ea0812bf0bf89ee6af93
Pascal <pascalokereke18@gmail.com>
2023-09-10 20:54:25 +0100

    done

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const genBtn = document.querySelector('.btn1');
 const copyBtn = document.querySelector('.btn2');
-let password = document.getElementById('password');
+
 
 genBtn.addEventListener('click', () => genPassword());
 copyBtn.addEventListener('click', () => copyPassword());
+
+function genPassword() {
+    let chars =
+     "012345678abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    passwordLength = 7;
+    password = '';
+
+
+    for(let i = 0; i <= passwordLength; i++) {
+        let randomNumber = Math.floor(Math.random() * chars.length)
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+    let copyText = document.getElementById("password");
+    copyText.select()
+    document.execCommand("Copy");
+     
+}
